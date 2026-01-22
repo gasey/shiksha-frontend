d9abaf505cafa3dd26fbbca74307b2abf7df75a9
Hruaia <hruaitluangasailo20@gmail.com>
2026-01-22 23:53:17 +0530

    hellosd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // 🔴 THIS WAS MISSING
+  base: "/",          // REQUIRED for DigitalOcean root domain
   plugins: [react()],
   resolve: {
     alias: {
